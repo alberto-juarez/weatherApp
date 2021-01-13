25d33c5ce7aaaf318928b36f56943878f74078d7
Alberto Juarez <aji147@gmail.com>
2021-01-13 17:47:24 -0600

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Alberto Juarez on 13/01/21.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
